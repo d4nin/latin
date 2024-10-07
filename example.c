633271ad50cbd0c051e79d013ed11339e2f7553c
Danin Sadžak <daninsadzak@gmail.com>
2024-10-07 20:56:57 +0200

    update example.c

--- example.c
+++ example.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "latin/conjugation.h"
 
 int main() {
-    printf("Hello, world!\n");
+    struct Conjugation conjugation = {"audire", FOURTH};
+    // Get the base of the given verb.
+    printf("%s", getBaseOfVerb(conjugation));
     return 0;
 }
