1a46d7e033b1d13123a1294b91b036bc2853d6af
Danin Sadžak <daninsadzak@gmail.com>
2024-10-08 20:46:49 +0200

    update example.c

--- example.c
+++ example.c
@@ -2,8 +2,11 @@
 #include "latin/conjugation.h"
 
 int main() {
-    struct Conjugation conjugation = {"amare", FIRST};
+    struct Conjugation conjugation = {"audire", FOURTH, THIRD_PERSON, PLURAL};
+    char *example = conjugate(conjugation);
+    // get the base of the verb
     printf("%s\n", getBaseOfVerb(conjugation));
-    conjugate1st(conjugation);
+    // print out specific conjugation based on type, person and number
+    printf("%s\n", example);
     return 0;
 }
