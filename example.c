26ef6a20a840585fa233235285da1df530964478
Danin Sadžak <daninsadzak@gmail.com>
2024-10-09 21:45:43 +0200

    update example.c

--- example.c
+++ example.c
@@ -2,20 +2,28 @@
 #include "latin/conjugation.h"
 
 int main() {
-    struct Conjugation conjugation = {"audire", FOURTH, THIRD_PERSON, SINGULAR};
+    struct Conjugation conjugation = {"amare", FIRST, THIRD_PERSON, SINGULAR};
     char *example = conjugate(conjugation);
     // get the base of the verb:
     printf("%s\n", getBaseOfVerb(conjugation));
+
     // print out specific conjugation based on type, person and number:
     printf("%s\n", example);
+
     // check if the conjugation is of first type:
     if (isFirstConjugation(conjugation)) printf("true\n");
     else printf("false\n");
+
     // check if the conjugation is of second type:
     if (isSecondConjugation(conjugation)) printf("true\n");
     else printf("false\n");
-    // check if the conjugaiton is of third type:
+
+    // check if the conjugation is of third type:
     if (isThirdConjugation(conjugation)) printf("true\n");
     else printf("false\n");
+
+    // check if the conjugation is of fourth type:
+    if (isFourthConjugation(conjugation)) printf("truer\n");
+    else printf("falser\n");
     return 0;
 }
