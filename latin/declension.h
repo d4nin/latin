b283249324da7753030e3748eca0724942be9ac7
Danin Sadžak <daninsadzak@gmail.com>
2024-10-11 18:00:17 +0200

    implement declineNoun() for first type of declension

--- latin/declension.h
+++ latin/declension.h
@@ -14,9 +14,14 @@ enum Gender {
     NEUTRUM
 };
 
+enum Number {
+    SINGULAR,
+    PLURAL
+};
+
 enum Case {
     NOMINATIVE,
-    GENETIVE,
+    GENITIVE,
     DATIVE,
     ACCUSATIVE,
     VOCATIVE,
@@ -27,6 +32,7 @@ struct Declension {
     char *noun;
     enum declensionType type;
     enum Gender gender;
+    enum Number number;
     enum Case casus; 
 };
 
@@ -41,5 +47,60 @@ char *getBaseOfNoun(struct Declension declension) {
         case FIRST:
             copy[length - 1] = '\0';
             return copy;
+    }
+}
+
+char *declineNoun(struct Declension declension) {
+    char *base = getBaseOfNoun(declension);
+    char *declinedNoun;
+
+    switch (declension.type) {
+        case FIRST:
+            if (declension.gender == NEUTRUM)
+                return "noun cannot be of gender neutrum.";
+            if (declension.number == SINGULAR) {
+                switch (declension.casus) {
+                    case NOMINATIVE:
+                        declinedNoun = strcat(base, "a");
+                        return declinedNoun;
+                    case GENITIVE:
+                        declinedNoun = strcat(base, "ae");
+                        return declinedNoun;
+                    case DATIVE:
+                        declinedNoun = strcat(base, "ae");
+                        return declinedNoun;
+                    case ACCUSATIVE:
+                        declinedNoun = strcat(base, "am");
+                        return declinedNoun;
+                    case VOCATIVE:
+                        declinedNoun = strcat(base, "a");
+                        return declinedNoun;
+                    case ABLATIVE:
+                        declinedNoun = strcat(base, "a");
+                        return declinedNoun;
+                }
+            }
+            if (declension.number == PLURAL) {
+                switch (declension.casus) {
+                    case NOMINATIVE:
+                        declinedNoun = strcat(base, "ae");
+                        return declinedNoun;
+                    case GENITIVE:
+                        declinedNoun = strcat(base, "arum");
+                        return declinedNoun;
+                    case DATIVE:
+                        declinedNoun = strcat(base, "is");
+                        return declinedNoun;
+                    case ACCUSATIVE:
+                        declinedNoun = strcat(base, "as");
+                        return declinedNoun;
+                    case VOCATIVE:
+                        declinedNoun = strcat(base, "ae");
+                        return declinedNoun;
+                    case ABLATIVE: 
+                        declinedNoun = strcat(base, "is");
+                        return declinedNoun;
+                }  
+            }
     }
 }
