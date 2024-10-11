f2b208ead1c2ddeafdf2c8cbdf168d6c59306ace
Danin Sadžak <daninsadzak@gmail.com>
2024-10-11 18:01:49 +0200

    update example_declension.c

--- examples/example_declension.c
+++ examples/example_declension.c
@@ -1,9 +1,12 @@
 #include "../latin/declension.h"
 
 int main() {
-    struct Declension declension = {"terra", FIRST, FEMININUM, NOMINATIVE};
+    struct Declension declension = {"terra", FIRST, NEUTRUM, PLURAL, ABLATIVE};
 
     // get the noun's base
     printf("%s\n", getBaseOfNoun(declension));
+
+    // decline the given noun
+    printf("%s\n", declineNoun(declension));
     return 0;
 }
