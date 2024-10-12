d8388cf1e8d7521fd9cae70903b32047291820d1
Danin Sadžak <daninsadzak@gmail.com>
2024-10-12 21:41:14 +0200

    update example_declension.c

--- examples/example_declension.c
+++ examples/example_declension.c
@@ -2,9 +2,14 @@
 
 int main() {
     // setting up noun of first declension
-    struct declensionFirst first = {"terra", FEMININUM, SINGULAR, NOMINATIVE};
-    
+    struct declensionFirst first = {"terra", NEUTRUM, SINGULAR, NOMINATIVE};
+
     // get the base of noun of first declension
     char *baseFirst = getStemOfNounFirstDeclension(first);
     printf("%s\n", baseFirst);
+
+    // check if noun is of first declension
+    if (isNounFirstDeclension(first))
+        printf("it true\n");
+    else printf("it false :(\n");
 }
