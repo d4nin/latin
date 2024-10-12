8e47b877be74f1ba5c3e8fc2c7c42733bfa66e0f
Danin Sadžak <daninsadzak@gmail.com>
2024-10-13 00:05:41 +0200

    [REFACTOR] update example_declension.c

--- examples/example_declension.c
+++ examples/example_declension.c
@@ -1,18 +1,33 @@
 #include "../latin/declension.h"
 
-int main() {
+void firstDeclension() {
     // setting up noun of first declension
-    struct declensionFirst first = {"terra", FEMININUM, PLURAL, ABLATIVE};
+    struct declensionFirst first = {"terra", NEUTRUM, PLURAL, ABLATIVE};
+
+    // check if the noun is of first declension
+    if (isNounFirstDeclension(first))
+        printf("True!\n");
+    else printf("False!\n");
 
     // get the base of noun of first declension
     char *baseFirst = getStemOfNounFirstDeclension(first);
     printf("%s\n", baseFirst);
 
-    // check if noun is of first declension
-    if (isNounFirstDeclension(first))
-        printf("it true\n");
-    else printf("it false :(\n");
-
     // decline the noun of first declension
     printf("%s\n", declineNounFirstDeclension(first));
+}
+
+void secondDeclension() {
+    // setting up noun of second declension
+    struct declensionSecond second = {"bellum", MASCULINUM, SINGULAR, NOMINATIVE, 0};
+    // check if the noun is of second declension
+    if (isNounSecondDeclension(second))
+        printf("Correct!\n");
+    else printf("Incorrect!\n");
+}
+
+int main() {
+    firstDeclension();
+    secondDeclension();
+    return 0;
 }
