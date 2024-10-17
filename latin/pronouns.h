826b057d4526d1111ca158b94b1bf3d598c0edb6
Danin Sadžak <daninsadzak@gmail.com>
2024-10-17 20:13:39 +0200

    implement getPersonalPronoun()

--- latin/pronouns.h
+++ latin/pronouns.h
@@ -32,3 +32,98 @@ struct pronouns {
     enum Case casus;
     enum Gender gender;
 };
+
+char *getPersonalPronoun(struct pronouns pronoun) {
+    char *personalPronoun;
+    if (pronoun.casus == VOCATIVE)
+        return "personal pronouns don't have a vocative form!";
+    if (pronoun.person == FIRST_PERSON && pronoun.number == SINGULAR) {
+        switch (pronoun.casus) {
+            case ACCUSATIVE:
+            case ABLATIVE:
+                personalPronoun = "me";
+                return personalPronoun;
+            case NOMINATIVE:
+                personalPronoun = "ego";
+                return personalPronoun;
+            case GENITIVE:
+                personalPronoun = "mei";
+                return personalPronoun;
+            case DATIVE:
+                personalPronoun = "mihi";
+                return personalPronoun;
+        }
+    }
+    if (pronoun.person == FIRST_PERSON && pronoun.number == PLURAL) {
+        switch (pronoun.casus) {
+            case NOMINATIVE:
+            case ACCUSATIVE:
+                personalPronoun = "nos";
+                return personalPronoun;
+            case DATIVE:
+            case ABLATIVE:
+                personalPronoun = "nobis";
+                return personalPronoun;
+            case GENITIVE:
+                personalPronoun = "nostri";
+                return personalPronoun;
+        }
+    }
+    if (pronoun.person == SECOND_PERSON && pronoun.number == SINGULAR) {
+        switch (pronoun.casus) {
+            case ACCUSATIVE:
+            case ABLATIVE:
+                personalPronoun = "te";
+                return personalPronoun;
+            case NOMINATIVE:
+                personalPronoun = "tu";
+                return personalPronoun;
+            case GENITIVE:
+                personalPronoun = "tui";
+                return personalPronoun;
+            case DATIVE:
+                personalPronoun = "tihi";
+                return personalPronoun;
+        }
+    }
+    if (pronoun.person == SECOND_PERSON && pronoun.number == PLURAL) {
+        switch (pronoun.casus) {
+            case NOMINATIVE:
+            case ACCUSATIVE:
+                personalPronoun = "vos";
+                return personalPronoun;
+            case DATIVE:
+            case ABLATIVE:
+                personalPronoun = "vobis";
+                return personalPronoun;
+            case GENITIVE:
+                personalPronoun = "vostri";
+                return personalPronoun;
+        }
+    }
+    if (pronoun.person == THIRD_PERSON && pronoun.number == SINGULAR) {
+        switch (pronoun.gender) {
+            case MASCULINUM:
+                personalPronoun = "is";
+                return personalPronoun;
+            case FEMININUM:
+                personalPronoun = "ea";
+                return personalPronoun;
+            case NEUTRUM:
+                personalPronoun = "id";
+        } 
+    }
+    if (pronoun.person == THIRD_PERSON && pronoun.number == PLURAL) {
+        switch (pronoun.gender) {
+            case MASCULINUM:
+                personalPronoun = "ii";
+                return personalPronoun;
+            case FEMININUM:
+                personalPronoun = "eae";
+                return personalPronoun;
+            case NEUTRUM:
+                personalPronoun = "ea";
+                return personalPronoun;
+        }
+    }
+}
