ff1b723578e23a2ac79acf150fb3bd608da6b98c
Danin Sadžak <daninsadzak@gmail.com>
2024-10-19 14:24:45 +0200

    update example_pronouns.c

--- examples/example_pronouns.c
+++ examples/example_pronouns.c
@@ -2,7 +2,7 @@
 
 int main() {
     // setup "pronouns" struct
-    struct pronouns pronoun = {THIRD_PERSON, PLURAL, ABLATIVE, NEUTRUM};
+    struct pronouns pronoun = {FIRST_PERSON, PLURAL, ACCUSATIVE, NEUTRUM};
 
     // get the personal pronoun
     char *personalPronoun = getPersonalPronoun(pronoun);
@@ -11,5 +11,12 @@ int main() {
     // get the reflexive pronoun
     char *reflexivePronoun = getReflexivePronoun(pronoun);
     printf("%s\n", reflexivePronoun);
+
+    // setup demonstrative pronouns struct
+    struct demonstrativePronouns pronouns = {pronoun, "idem, eadem, idem"};
+
+    // get the demonstrative pronoun 
+    char *demonstrativePronoun = getDemonstrativePronoun(pronouns);
+    printf("%s\n", demonstrativePronoun);
     return 0;
 }
