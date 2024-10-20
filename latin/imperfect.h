e703ce93995806c4bccd7f9f37eb20c7a7a3aeae
Danin Sadžak <daninsadzak@gmail.com>
2024-10-21 00:25:05 +0200

    conjugateVerbPerfect() - implement for third and fourth conjugation

--- latin/imperfect.h
+++ latin/imperfect.h
@@ -38,6 +38,37 @@ char *conjugateVerbPerfect(struct Conjugation conjugation) {
                 }
         }
     }
+    if (isThirdConjugation(conjugation) || isFourthConjugation(conjugation)) {
+        switch (conjugation.Number) {
+            case SINGULAR:
+                if (conjugation.Person == FIRST_PERSON) {
+                    conjugatedVerb = strcat(stem, "ebam");
+                    return conjugatedVerb;
+                }
+                if (conjugation.Person == SECOND_PERSON) {
+                    conjugatedVerb = strcat(stem, "ebas");
+                    return conjugatedVerb;
+                }
+                if (conjugation.Person == THIRD_PERSON) {
+                    conjugatedVerb = strcat(stem, "ebat");
+                    return conjugatedVerb;
+                }
+            case PLURAL:
+                if (conjugation.Person == FIRST_PERSON) {
+                    conjugatedVerb = strcat(stem, "ebamus");
+                    return conjugatedVerb;
+                }
+                if (conjugation.Person == SECOND_PERSON) {
+                    conjugatedVerb = strcat(stem, "ebatis");
+                    return conjugatedVerb;
+                }
+                if (conjugation.Person == THIRD_PERSON) {
+                    conjugatedVerb = strcat(stem, "ebant");
+                    return conjugatedVerb;
+                }
+        }
+    }
+    return "";
 }
 
 #endif
