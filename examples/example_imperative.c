b3813dd09cd251dac3552415febf7bce25e9f8da
Danin Sadžak <daninsadzak@gmail.com>
2024-10-21 23:37:10 +0200

    update example_imperative.c

--- examples/example_imperative.c
+++ examples/example_imperative.c
@@ -1,9 +1,12 @@
 #include "../latin/imperative.h"
 
 int main() {
-    struct Conjugation conjugation = {"scribere", THIRD, SECOND_PERSON, PLURAL};
+    struct Conjugation conjugation = {"scribere", THIRD, THIRD_PERSON, PLURAL};
 
     // get the imperative first form
     printf("%s\n", imperativeFirst(conjugation));
+
+    // get the imperative (future) second form
+    printf("%s\n", imperativeSecond(conjugation)); 
     return 0;
 }
