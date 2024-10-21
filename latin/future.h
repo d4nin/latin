c0a9c92b4dc9e8a3bae8af8504f9d88156b59228
Danin Sadžak <daninsadzak@gmail.com>
2024-10-21 17:22:00 +0200

    conjugateVerbFuture() - implement for third and fourth conjugation

--- latin/future.h
+++ latin/future.h
@@ -38,6 +38,37 @@ char *conjugateVerbFuture(struct Conjugation conjugation) {
                 }
         }
     }
+    if (isThirdConjugation(conjugation) || isFourthConjugation(conjugation)) {
+        switch (conjugation.Number) {
+            case SINGULAR:
+                if (conjugation.Person == FIRST_PERSON) {
+                    conjugatedVerb = strcat(stem, "am");
+                    return conjugatedVerb;
+                }
+                if (conjugation.Person == SECOND_PERSON) {
+                    conjugatedVerb = strcat(stem, "es");
+                    return conjugatedVerb;
+                }
+                if (conjugation.Person == THIRD_PERSON) {
+                    conjugatedVerb = strcat(stem, "et");
+                    return conjugatedVerb;
+                }
+            case PLURAL:
+                if (conjugation.Person == FIRST_PERSON) {
+                    conjugatedVerb = strcat(stem, "emus");
+                    return conjugatedVerb;
+                }
+                if (conjugation.Person == SECOND_PERSON) {
+                    conjugatedVerb = strcat(stem, "etis");
+                    return conjugatedVerb;
+                }
+                if (conjugation.Person == THIRD_PERSON) {
+                    conjugatedVerb = strcat(stem, "ent");
+                    return conjugatedVerb;
+                }
+        }
+    }
+    return "";
 }
 
 #endif
