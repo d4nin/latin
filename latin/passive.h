a7854773ae1e6cac7c9411401f594eac7d932517
Danin Sadžak <daninsadzak@gmail.com>
2024-10-25 23:26:08 +0200

    implement isVerbPassiveVoice()

--- latin/passive.h
+++ latin/passive.h
@@ -105,4 +105,59 @@ char *conjugateVerbPassive(struct Conjugation conjugation) {
     return conjugatedVerb;
 }
 
+bool isVerbPassiveVoice(char *verb) {
+    char *copyVerb = (char *)malloc((strlen(verb) + 1) * sizeof(char));
+    strcpy(copyVerb, verb);
+    int length = strlen(copyVerb);
+
+    char lastTwo[3];
+    lastTwo[2] = '\0';
+    
+    for (int i = 0; i < 2; i++) 
+        lastTwo[i] = copyVerb[length - 2 + i];
+
+    if (strcmp(lastTwo, "or") == 0) {
+        free(copyVerb);
+        return true;
+    }
+    
+    char lastThree[4];
+    lastThree[3] = '\0';
+
+    for (int i = 0; i < 3; i++)
+        lastThree[i] = copyVerb[length - 3 + i];
+
+    if (   strcmp(lastThree, "ris") == 0 || strcmp(lastThree, "tur") == 0
+        || strcmp(lastThree, "mur") == 0) {
+        free(copyVerb);
+        return true;
+    }
+
+    char lastFour[5];
+    lastFour[4] = '\0';
+
+    for (int i = 0; i < 4; i++)
+        lastFour[i] = copyVerb[length - 4 + i];
+
+    if (   strcmp(lastFour, "mini") == 0 || strcmp(lastFour, "ntur") == 0
+        || strcmp(lastFour, "eris") == 0 || strcmp(lastFour, "itur") == 0
+        || strcmp(lastFour, "imur") == 0) {
+        free(copyVerb);
+        return true;
+    }
+
+    char lastFive[6];
+    lastFive[5] = '\0';
+
+    for (int i = 0; i < 5; i++) 
+        lastFive[i] = copyVerb[length - 5 + i];
+
+    if (strcmp(lastFive, "imini") == 0 || strcmp(lastFive, "untur") == 0) {
+        free(copyVerb);
+        return true;
+    }
+    
+    return false;
+}
+
 #endif
