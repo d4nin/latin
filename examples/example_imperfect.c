aa0e50673a9b00b2cacb2a3e8e586192a5d52156
Danin Sadžak <daninsadzak@gmail.com>
2024-10-26 21:39:45 +0200

    update example_imperfect.c

--- examples/example_imperfect.c
+++ examples/example_imperfect.c
@@ -2,9 +2,12 @@
 #include "../latin/imperfect.h"
 
 int main() {
-    struct Conjugation conjugation = {"audire", FOURTH, FIRST_PERSON, SINGULAR};
+    struct Conjugation conjugation = {"audire", FOURTH, THIRD_PERSON, PLURAL};
 
     // get the verb's stem in the imperfect tense
     char *stemImperfect = getStemOfVerbImperfect(conjugation);
     printf("%s\n", stemImperfect);
+
+    char *conjugationImperfect = conjugateVerbImperfect(conjugation);
+    printf("%s\n", conjugationImperfect);
 }
