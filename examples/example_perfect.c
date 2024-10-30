7f697cd79c25f0ae34270eebc295dadd97f13b26
Danin Sadžak <daninsadzak@gmail.com>
2024-10-30 18:29:53 +0100

    update example_perfect.c

--- examples/example_perfect.c
+++ examples/example_perfect.c
@@ -2,11 +2,14 @@
 #include "../latin/perfect.h"
 
 int main() {
-    struct Conjugation conjugation = {"scribere", THIRD, THIRD_PERSON, PLURAL};
+    struct Conjugation conjugation = {"habere", SECOND, FIRST_PERSON, PLURAL};
 
     // conjugate the verb in the first type of the perfect tense
     char *verbPerfectFirstType = conjugateVerbPerfectFirstType(conjugation);
     printf("%s\n", verbPerfectFirstType);
 
+    // conjugate the verb in the second type of the perfect tense
+    char *verbPerfectSecondType = conjugateVerbPerfectSecondType(conjugation);
+    printf("%s\n", verbPerfectSecondType);
     return 0;
 }
