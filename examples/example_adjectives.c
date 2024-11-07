1313144ea44041feb80dbead75a87c95010d1530
Danin Sadžak <daninsadzak@gmail.com>
2024-11-07 21:20:38 +0100

    update example_adjectives.c

--- examples/example_adjectives.c
+++ examples/example_adjectives.c
@@ -2,6 +2,9 @@
 #include "../latin/declension.h"
 
 int main(void) {
-    
+    struct declension noun1 = {"patria", FEMININUM, SINGULAR, NOMINATIVE};
+    struct declensionSecond noun2 = {"bellum", NEUTRUM, SINGULAR, NOMINATIVE, 1};
+    printf("%d\n", getGenderOfAdjective(noun1.noun));
+    printf("%d\n", getGenderOfAdjective(noun2.noun));
     return 0;
 }
