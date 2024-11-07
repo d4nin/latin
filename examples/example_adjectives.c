5abab27824ce29defb974937cd8d09f3c6b6b141
Danin Sadžak <daninsadzak@gmail.com>
2024-11-07 21:46:28 +0100

    update example_adjectitves.c

--- examples/example_adjectives.c
+++ examples/example_adjectives.c
@@ -2,9 +2,17 @@
 #include "../latin/declension.h"
 
 int main(void) {
+    // setting up the adjective struct
+    struct adjectives adjective = {"bonus", 2};
+
+    // get the adjective's gender (for first and second declension)
     struct declension noun1 = {"patria", FEMININUM, SINGULAR, NOMINATIVE};
-    struct declensionSecond noun2 = {"bellum", NEUTRUM, SINGULAR, NOMINATIVE, 1};
+    struct declensionSecond noun2 = {"populus", MASCULINUM, SINGULAR, NOMINATIVE, 1};
     printf("%d\n", getGenderOfAdjective(noun1.noun));
     printf("%d\n", getGenderOfAdjective(noun2.noun));
+
+    // get the adjective's stem (for the first and second declension)
+    char *stem = getStemOfAdjective(adjective, noun2.noun);
+    printf("%s\n", stem);
     return 0;
 }
