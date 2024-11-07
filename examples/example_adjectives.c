762201d23e0732df0638c64558d09751e9eb35b6
Danin Sadžak <daninsadzak@gmail.com>
2024-11-07 22:03:23 +0100

    update example_adjectives.c

--- examples/example_adjectives.c
+++ examples/example_adjectives.c
@@ -3,10 +3,10 @@
 
 int main(void) {
     // setting up the adjective struct
-    struct adjectives adjective = {"bonus", 2};
+    struct adjectives adjective = {"bonus", 1, FEMININUM, PLURAL, ABLATIVE};
 
     // get the adjective's gender (for first and second declension)
-    struct declension noun1 = {"patria", FEMININUM, SINGULAR, NOMINATIVE};
+    struct declension noun1 = {"patria", FEMININUM, PLURAL, ABLATIVE};
     struct declensionSecond noun2 = {"populus", MASCULINUM, SINGULAR, NOMINATIVE, 1};
     printf("%d\n", getGenderOfAdjective(noun1.noun));
     printf("%d\n", getGenderOfAdjective(noun2.noun));
@@ -14,5 +14,8 @@ int main(void) {
     // get the adjective's stem (for the first and second declension)
     char *stem = getStemOfAdjective(adjective, noun2.noun);
     printf("%s\n", stem);
+
+    // decline the adjective of first declension
+    printf("%s %s\n", declineAdjectiveFirstDeclension(adjective, noun1.noun), declineNounFirstDeclension(noun1));
     return 0;
 }
